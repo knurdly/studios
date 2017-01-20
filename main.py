4b2e46f7617df1e7cbdac40dede4ca56c6372bfa
Mauricio <mlozano0322@gmail.com>
2017-01-19 21:08:37 -0600

    this is my message

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import webapp2
+import random
 
 class Index(webapp2.RequestHandler):
 
-    def getRandomMovie(self):
+    def get_random_movie(self):
 
         # TODO: make a list with at least 5 movie titles
-
+        movies = [
+            "asdf",
+            "asdfasdf",
+            "asdlfkjw"
+        ]
         # TODO: randomly choose one of the movies, and return it
-
-        return "The Big Lebowski"
+        random_movie_index = random.randomranger(len(movies))
+        return movies(random_movie_index)
 
     def get(self):
         # choose a movie by invoking our new function
-        movie = self.getRandomMovie()
+        movie = self.get_random_movie()
 
         # build the response string
         content = "<h1>Movie of the Day</h1>"
@@ -20,7 +25,9 @@ class Index(webapp2.RequestHandler):
 
         # TODO: pick a different random movie, and display it under
         # the heading "<h1>Tommorrow's Movie</h1>"
-
+        tomorrw_movie = self.get_random_movie
+        content+="<h1>asdfasdfjlkdsfa</h1>"
+        content+="<p>"  + movie +"</p>""
         self.response.write(content)
 
 app = webapp2.WSGIApplication([
